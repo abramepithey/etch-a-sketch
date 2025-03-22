ac4b43fd5de13fab4a10f4bb2fe6ea7f05b94d02
abramepithey <abramepithey@gmail.com>
2025-03-21 19:44:17 -0600

    Add initial javascript pseudocode

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+draw method takes in number of sides
+    get document body
+    loop number of sides times
+        create new square div row
+        loop number of sides times
+            create new square div
+            append square to square row 
+        append square row div to body
+
+add click listener to button to fire off draw method
+add mouseover listener to squares
+on mouseover = () => {}
+    apply dark styling to square
+*/
